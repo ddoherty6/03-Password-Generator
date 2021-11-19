279db0c4c659eadd0dc660e663befe26b438431d
klem <ddoherty6@gmail.com>
2021-11-18 23:11:27 -0800

    first attempt at building an object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var Generate = {
+
+  passwordLength: "",
+
+
+  function forceInt() { //interact with user until input requirements are met
+
+    while (parseInt(this.passwordLength) === NaN) { //passwordLength needs to be a number
+      this.passwordLength = prompt("How many characters do you want in your password?");
+      if (this.passwordLength <8 || this.passwordLength > 128) { //passwordLength needs to be a number 8-128
+        alert("I can only do a password between 8 and 128 characters long - please enter again");
+      } else {
+        alert("Please enter a valid number");
+      }
+    }
+  },
+
+
+  function generatePassword() {
+
+    console.log(this.passwordLength);
+  }
+}
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generate.Password();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
