d09e377802fb81e47a171c35fa281b50a6077ccf
klem <ddoherty6@gmail.com>
2021-11-21 18:00:43 -0800

    cleaning up code, adding comments where needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var generateBtn = document.querySelector("#generate");
 
-var userInput = {
+var userInput = { // object to handle all interaction with the user
 
   passwordLength: null,
   wantsLowerCase: null,
@@ -8,14 +8,14 @@ var userInput = {
   wantsNumeric: null,
   wantsSpecialChars: null,
 
-  getLength: function() { //interact with user until input requirements are met
+  getLength: function() { // ask user how long they want the password to be, enforcing interger [8, 128]
     while (!Number.isInteger(this.passwordLength) || (this.passwordLength < 8 || this.passwordLength > 128)) { //passwordLength needs to be an integer 8-128
       input = window.prompt("How many characters would you like in your password? Please enter an integer between 8 and 128");
       this.passwordLength = Number.parseInt(input);
     }
   },
 
-  getCharSet: function() {
+  getCharSet: function() { // promt user for which types of chars they want - do not allow them to choose none
     this.wantsLowerCase = confirm("Would you like your password to have lower case letters?");
     if (this.wantsLowerCase === true) {
       alert("Including lower case letters.");
@@ -51,8 +51,8 @@ var userInput = {
   }
 }
 
-gen = { //i am not calling this 'generate' because that is the id of the button element-object
-  pswrd: Array(), //naming pswrd so as not to conflict with password class html element-object
+gen = { // object to handle password generation - i am not calling this 'generate' because that is the id of the button element-object
+  pswrd: Array(), // naming 'pswrd' so as not to conflict with 'password' element-object
   hasNum: false,
   hasUpper: false,
   hasLower: false,
@@ -60,23 +60,7 @@ gen = { //i am not calling this 'generate' because that is the id of the button 
   randNum: 0,
   meetsCriteria: false,
 
-  /*
-  trackCharTypes: function(letter) { //takes an ascii index argument and checks whether it is numeric, upper case, lowercase, or special
-    if (letter >= 48 && letter <= 57) {
-      this.hasNum = true;
-    }
-    else if (letter >= 65 && letter <= 90) {
-      this.hasUpper = true;
-    }
-    else if (letter >= 97 && letter <= 122) {
-      this.hasLower = true;
-    }
-    else {
-      this.hasSpecial = true;
-    }
-  }, */
-
-  criteriaCheck: function() { //check that password has or doesnt have the types of charcters users specify
+  criteriaCheck: function() { // check that full array 'pswrd' has or doesnt have ALL types of charcters users specify
     if (this.hasLower == userInput.wantsLowerCase && this.hasUpper == userInput.wantsUpperCase && this.hasNum == userInput.wantsNumeric && this.hasSpecial == userInput.wantsSpecialChars) {
       this.meetsCriteria = true;
     } else {
@@ -84,7 +68,7 @@ gen = { //i am not calling this 'generate' because that is the id of the button 
     }
   },
 
-  currentCharMeetsCriteria: function(ASCIIindex) { //checks to see if (index of) char in question meets criteria specified by the user
+  currentCharMeetsCriteria: function(ASCIIindex) { // checks to see if (index of) char in question meets criteria specified by the user, keeps track of types of chars used
     if (ASCIIindex >= 48 && ASCIIindex <= 57 && userInput.wantsNumeric) {
       this.hasNum = true;
       return true;
@@ -115,38 +99,28 @@ gen = { //i am not calling this 'generate' because that is the id of the button 
     this.meetsCriteria = false;
   },
   
-  makePassword: function() {
-    while (!this.meetsCriteria) { //keep generating passwords until one meets criteria set by the user
-      this.resetPswrd(); //reset pswrd to prepare it for the loop
-debugger;
-      for(i=0; i<userInput.passwordLength; i++) {
-        
+  makePassword: function() { // password generation driver
+    while (!this.meetsCriteria) { // keep generating passwords until full pswrd array meets criteria set by the user
+      this.resetPswrd(); // reset pswrd and other object vars to prepare for the genration loop
+
+      for(i=0; i<userInput.passwordLength; i++) { // this loop generates the password   
         this.randNum = parseInt(Math.random()*94+32); //generate random number [32, 126], which is the range of characters we want in ascii index
         this.pswrd.push(String.fromCharCode(this.randNum)); //obtain ascii char with that index, assign to array
 
-          while (!this.currentCharMeetsCriteria(this.randNum)) { //check that (index of) current char meets criteria set by user, if not, regen the number at array index and check again
-            this.randNum = parseInt(Math.random()*94+32);
-            this.pswrd[i] = String.fromCharCode(this.randNum);
-            console.log("check char");
-          }
-        //this.trackCharTypes(this.randNum); //check the entire array to make sure all requirements are met
+        while (!this.currentCharMeetsCriteria(this.randNum)) { //check that (index of) current char meets criteria set by user, if not, regen the number at array index and check again
+          this.randNum = parseInt(Math.random()*94+32);
+          this.pswrd[i] = String.fromCharCode(this.randNum);
+        }
       }
-
-      this.criteriaCheck();
+      this.criteriaCheck(); //check that all user-specified criteria are met, as above logic may generate password with 2 of the 3 selected char types, etc
     }
-    return this.pswrd; //give the people what they want
+    return this.pswrd;
   }
 } 
 
-
-
-
-
-console.log('before writePassword');
 // Write password to the #password input
 function writePassword() {
 
-  
   var password = gen.makePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password.join("");
@@ -154,10 +128,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-console.log('before generateBtn');
-//generateBtn.addEventListener("click", writePasswrod); //had to comment this out - it was running the writePassword function at this line upon click
-console.log('after generateBtn');
-
+//generateBtn.addEventListener("click", writePassword); //had to comment this out - it was running the writePassword function at this line upon click
 
 generate.onclick = function () {
  
